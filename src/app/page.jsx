a05dc284149e416f3b7d0a5d42d39118e7fb5c39
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2023-06-12 22:37:28 +0300

    Created home-page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
-
-export default function Home() {
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      BLOG
-    </main>
-  )
+    <div className="min-h-screen flex items-center">
+      <div className="container">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-7xl font-semibold mb-5">Mehmet Ali Ayvaz</h1>
+          <h2 className="text-2xl font-medium">
+            Frontend Developer - Computer Engineer
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+        },
+      },
+      colors: {
+        dark: "rgb(11, 11, 15)",
+      },
+      backgroundImage: {},
+      fontFamily: {
+        georama: ["Georama", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
