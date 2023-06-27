d101ed90401cd4230d3410e5d5c5b757c0b5b948
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2023-06-27 14:53:16 +0300

    Created AppLayout

---
+++ src/app/layouts/AppLayout.jsx
@@ -0,0 +1,17 @@
+function AppLayout({ title, description, children }) {
+  return (
+    <div className="py-40">
+      <div className="container">
+        <div>
+          <div className="text-center mb-20">
+            <h1 className="text-4xl font-semibold mb-5">{title}</h1>
+            <p>{description}</p>
+          </div>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AppLayout;
