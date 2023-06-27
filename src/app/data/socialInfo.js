3b8cac3d8bd3c4191270ab99417cbc7601046186
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2023-06-27 16:45:19 +0300

    Added social-info data

---
+++ src/app/data/socialInfo.js
@@ -0,0 +1,54 @@
+import {
+  SiGithub,
+  SiLinkedin,
+  SiMedium,
+  SiTwitter,
+  SiInstagram,
+  SiYoutube,
+  SiPexels,
+} from "@icons-pack/react-simple-icons";
+
+const socialInfo = [
+  {
+    name: "Github",
+    icon: <SiGithub />,
+    link: "https://github.com/mehmetaliayvaz",
+  },
+  {
+    name: "Linkedin",
+    icon: <SiLinkedin />,
+    link: "https://www.linkedin.com/in/mehmetaliayvaz/",
+  },
+  {
+    name: "Medium",
+    icon: <SiMedium />,
+    link: "https://medium.com/@mehmetaliayvaz",
+  },
+  {
+    name: "Twitter",
+    icon: <SiTwitter />,
+    link: "https://twitter.com/mhmtaliayvaz",
+  },
+  {
+    name: "Instagram",
+    icon: <SiInstagram />,
+    link: "https://www.instagram.com/mhmtaliayvaz/",
+  },
+  {
+    name: "Youtube",
+    icon: <SiYoutube />,
+    link: "https://www.youtube.com/mehmetaliayvaz",
+  },
+  {
+    name: "Pexels",
+    icon: <SiPexels />,
+    link: "https://www.pexels.com/@mehmetaliayvaz",
+  },
+  {
+    name: "Instagram",
+    icon: <SiInstagram />,
+    link: "https://www.instagram.com/siyahdiyafram/",
+  },
+];
+
+export default socialInfo;
