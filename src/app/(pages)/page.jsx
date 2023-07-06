8909be4a4f3359433e2acfd5f992502a78aa96d5
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2023-07-06 23:42:44 +0300

    Edited home mobile view

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -3,8 +3,8 @@ function Home() {
     <div className="min-h-screen flex items-center">
       <div className="container">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-7xl font-semibold mb-5">Mehmet Ali Ayvaz</h1>
-          <h2 className="text-2xl font-medium">
+          <h1 className="text-5xl md:text-7xl font-semibold text-center mb-5">Mehmet Ali Ayvaz</h1>
+          <h2 className="text-2xl text-center font-medium">
             Frontend Developer - Computer Engineer
           </h2>
         </div>
